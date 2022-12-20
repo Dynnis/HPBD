1b6acea3090e47563e8a2bf4f0fe3e4ee837430e
Dynnis <hoigian99@gmail.com>
2022-12-20 20:18:26 +0700

    upadate

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ánh Nguyệt';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Món Quà Nhỏ';
+const contentCard = 'Sinh Nhật tuổi 19 vui vẻ và cười thật tươi nha. Chúc mọi điều tốt đẹp nhất sẽ đến với bạn vào hôm nay và cả những ngày sau, cuộc đời nhất định sẽ đối xử nhẹ nhàng với bạn nhiều chút. Khi nào có cảm xúc tiêu cực hãy đi ăn món gì đó thật ngon nha, nếu không có người đi cùng thì có thể gọi mình :> . Tận hưởng trọn vẹn tuổi 19 nhé!!';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
